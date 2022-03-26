8199215f44980c72940676a5323ca733ab10fe1c
khaliun1007 <lolitagothic7@gmail.com>
2022-03-26 13:22:25 -0500

    first file

---
+++ src/day0/First.java
@@ -0,0 +1,10 @@
+package day0;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Java");
+        System.out.println("working.....");
+
+        //checking comment section
+    }
+}
